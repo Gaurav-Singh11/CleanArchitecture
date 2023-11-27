eb5d21bb039e3d6510298cefd3858f5fb39184f7
Gaurav <gauravanilkumarsingh@gmail.com>
2023-11-27 11:04:20 +0530

    update

--- lib/src/on_boarding/domain/usecases/cache_first_timer.dart
+++ lib/src/on_boarding/domain/usecases/cache_first_timer.dart
@@ -1,4 +1,4 @@
-import 'package:tdd_flutter/core/usecases/usercases.dart';
+import 'package:tdd_flutter/core/usecases/usecases.dart';
 import 'package:tdd_flutter/core/utils/typedefs.dart';
 import 'package:tdd_flutter/src/on_boarding/domain/repos/on_boarding_repo.dart';
 
